01121037d49e18e24b04c37871d7caafc300e6e3
Linos Giannopoulos <lgian@element.io>
2023-07-12 12:22:29 +0300

    Set a random UDP port used by wireguard and Tailscale's discovery
    This will essentially retry until it finds an unused random high UDP port to use for
    Tailscale's Wireguard connections and discovery mechanism.
    This is the only way to get direct connections working on EKS currently.
    
    By default, `--port` is set to 0, which means that Tailscale will use a
    random UDP port, but it doesn't fall into this if statement, which is
    what we need (0), otherwise Tailscale will try all the other methods it
    uses to establish direct connections and none of them seem to work in
    our setup, so it falls back to using DERP.
    
    https://github.com/vector-im/sre-internal/issues/2563
    
    (0): https://github.com/tailscale/tailscale/blob/b3138a71ad0b2fb7d99f0dcecc5e4bc85483bb4f/wgengine/magicsock/magicsock.go#L1203

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 export PATH=$PATH:/tailscale/bin
 
+LOW_PORT=$(cat /proc/sys/net/ipv4/ip_local_port_range | cut -d '' -f1 )
+HIGH_PORT=$(cat /proc/sys/net/ipv4/ip_local_port_range | cut -d '' -f2 )
+
 AUTH_KEY="${AUTH_KEY:-}"
 ROUTES="${ROUTES:-}"
 EXTRA_ARGS="${EXTRA_ARGS:-}"
 USERSPACE="${USERSPACE:-true}"
+OUTBOUND_PROXY=${OUTBOUND_PROXY:-true}
+RANDOMIZE_PORT=${RANDOMIZE_PORT:-false}
 
 # If AUTH_KEY is not in env, attempt to find it under $AUTH_KEY_LOCAL_PATH
 if [ -z "${AUTH_KEY}" ] && [ ! -z  "${AUTH_KEY_LOCAL_PATH}" ];  then
@@ -15,34 +20,54 @@ set -e
 
 TAILSCALED_ARGS="--socket=/tmp/tailscaled.sock"
 
-if [[ "${USERSPACE}" == "true" ]]; then
-  TAILSCALED_ARGS="${TAILSCALED_ARGS} --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055"
-else
-  if [[ ! -d /dev/net ]]; then
-    mkdir -p /dev/net
-  fi
 
-  if [[ ! -c /dev/net/tun ]]; then
-    mknod /dev/net/tun c 10 200
-  fi
-fi
+while true
+do
+	if [[ "${RANDOMIZE_PORT}" == "true" ]]; then
+	  UDP_PORT=$(shuf -i ${LOW_PORT}-${HIGH_PORT} -n 1)
+	  TAILSCALED_ARGS="${TAILSCALED_ARGS} --port ${UDP_PORT}"
+	fi
 
-echo "Starting tailscaled"
-tailscaled ${TAILSCALED_ARGS} &
-PID=$!
+	if [[ "${OUTBOUND_PROXY}" == "true" ]]; then
+	  TAILSCALED_ARGS="${TAILSCALED_ARGS} --outbound-http-proxy-listen=localhost:1055"
+	fi
 
-UP_ARGS="--accept-dns=false"
-if [[ ! -z "${ROUTES}" ]]; then
-  UP_ARGS="--advertise-routes=${ROUTES} ${UP_ARGS}"
-fi
-if [[ ! -z "${AUTH_KEY}" ]]; then
-  UP_ARGS="--authkey=${AUTH_KEY} ${UP_ARGS}"
-fi
-if [[ ! -z "${EXTRA_ARGS}" ]]; then
-  UP_ARGS="${UP_ARGS} ${EXTRA_ARGS:-}"
-fi
+	if [[ "${USERSPACE}" == "true" ]]; then
+	  TAILSCALED_ARGS="${TAILSCALED_ARGS} --tun=userspace-networking"
+	else
+	  if [[ ! -d /dev/net ]]; then
+		mkdir -p /dev/net
+	  fi
 
-echo "Running tailscale up"
-tailscale --socket=/tmp/tailscaled.sock up ${UP_ARGS}
+	  if [[ ! -c /dev/net/tun ]]; then
+		mknod /dev/net/tun c 10 200
+	  fi
+	fi
+
+	echo "Starting tailscaled"
+	tailscaled ${TAILSCALED_ARGS} &
+	PID=$!
+
+	UP_ARGS="--accept-dns=false"
+	if [[ ! -z "${ROUTES}" ]]; then
+	  UP_ARGS="--advertise-routes=${ROUTES} ${UP_ARGS}"
+	fi
+	if [[ ! -z "${AUTH_KEY}" ]]; then
+	  UP_ARGS="--authkey=${AUTH_KEY} ${UP_ARGS}"
+	fi
+	if [[ ! -z "${EXTRA_ARGS}" ]]; then
+	  UP_ARGS="${UP_ARGS} ${EXTRA_ARGS:-}"
+	fi
+
+	echo "Running tailscale up"
+	tailscale --socket=/tmp/tailscaled.sock up ${UP_ARGS}
+	if [ $? -eq 0 ]
+	then
+		break;
+	else
+		sleep 1
+	fi
+done
+
 
 wait ${PID}
